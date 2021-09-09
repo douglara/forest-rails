f47cd6995972fbfaf3de908ad101f9a74897ef1b
Douglas Lara <douglasdelaras@gmail.com>
2021-09-09 13:39:48 -0300

    Get unscoped resources

--- app/services/forest_liana/base_getter.rb
+++ app/services/forest_liana/base_getter.rb
@@ -5,12 +5,7 @@ module ForestLiana
     end
 
     def get_resource
-      use_act_as_paranoid = @resource.instance_methods
-        .include? :really_destroyed?
-
-      # NOTICE: Do not unscope with the paranoia gem to prevent the retrieval
-      #         of deleted records.
-      use_act_as_paranoid ? @resource : @resource.unscoped
+      @resource.unscoped
     end
 
     def includes_for_serialization
